c15ff03cd62239bda9d8a7aeb0e4de5ec7029f96
Azmetov Danila <ohstapit@gmail.com>
2021-12-17 19:56:34 +0300

    Scripted auto-commit on change (2021-12-17 19:56:34) by gitwatch.sh

--- src/main/java/bmstu/rapirapr/azmetov/akka/TestActor.java
+++ src/main/java/bmstu/rapirapr/azmetov/akka/TestActor.java
@@ -14,6 +14,7 @@ public class TestActor extends AbstractActor {
     private static final String TEST_PASSED_STATUS = "PASSED";
     private static final String TEST_FAILED_STATUS = "FAILED";
     private static final String EXECUTION_FAILED_STATUS = "PASSED";
+    private static final String OUTPUT_INITIAL_VALUE = "";
 
     @Override
     public Receive createReceive() {
@@ -31,12 +32,13 @@ public class TestActor extends AbstractActor {
     }
 
     private TestResult test(MessageTest test) {
-        String status, result;
+        String status, output = OUTPUT_INITIAL_VALUE;
         try {
-            status = execJS(test.getJsScript(), test.getFunctionName(), test.getTest().getParams());
-            result = test.getTest().getExpectedResult() == 
+            output = execJS(test.getJsScript(), test.getFunctionName(), test.getTest().getParams());
+            status = test.getTest().getExpectedResult() == output ? TEST_PASSED_STATUS : TEST_FAILED_STATUS;
         } catch (ScriptException | NoSuchMethodException e) {
             status = EXECUTION_FAILED_STATUS;
         }
+        return new TestResult(test, status, output);
     }
 }
