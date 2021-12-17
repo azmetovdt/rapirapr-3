31b9eb98bf262a2693bd63e6cebf38cd5bd5ce5d
Azmetov Danila <ohstapit@gmail.com>
2021-12-17 19:44:52 +0300

    Scripted auto-commit on change (2021-12-17 19:44:52) by gitwatch.sh

--- src/main/java/bmstu/rapirapr/azmetov/akka/TestActor.java
+++ src/main/java/bmstu/rapirapr/azmetov/akka/TestActor.java
@@ -7,6 +7,7 @@ import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
+import java.util.List;
 
 public class TestActor extends AbstractActor {
 
@@ -18,7 +19,7 @@ public class TestActor extends AbstractActor {
                 }).build();
     }
 
-    public String execJS(String jscript, String functionName, Integer[] params) throws ScriptException, NoSuchMethodException {
+    public String execJS(String jscript, String functionName, List<Integer> params) throws ScriptException, NoSuchMethodException {
         ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
         engine.eval(jscript);
         Invocable invocable = (Invocable) engine;
