408ff667cf0465df8786f86feb65db702c290899
Azmetov Danila <ohstapit@gmail.com>
2021-12-17 18:36:47 +0300

    Scripted auto-commit on change (2021-12-17 18:36:47) by gitwatch.sh

--- src/main/java/bmstu/rapirapr/azmetov/akka/RouterActor.java
+++ src/main/java/bmstu/rapirapr/azmetov/akka/RouterActor.java
@@ -5,13 +5,18 @@ import akka.japi.pf.ReceiveBuilder;
 
 public class RouterActor extends AbstractActor {
 
+    private fi
+    public RouterActor() {
+
+    }
+
 
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(Message.class, m -> {
                     for (Test test : m.getTests()) {
-                        System.out.println(test);
+                        System.out.println(test.getTestName());
                     }
                 }).build();
     }
