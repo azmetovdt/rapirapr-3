103b3c9ee2a692d3959e2645e4194235175fc9c0
Azmetov Danila <ohstapit@gmail.com>
2021-12-17 20:34:42 +0300

    Scripted auto-commit on change (2021-12-17 20:34:42) by gitwatch.sh

--- src/main/java/bmstu/rapirapr/azmetov/akka/StoreActor.java
+++ src/main/java/bmstu/rapirapr/azmetov/akka/StoreActor.java
@@ -4,12 +4,13 @@ import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class StoreActor extends AbstractActor {
 
-    private Map<String, List<TestResult>> testResultsMap;
+    private Map<String, List<TestResult>> testResultsMap = new HashMap<>();
 
     @Override
     public Receive createReceive() {
