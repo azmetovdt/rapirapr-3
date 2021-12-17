401478cac0948754c78b6d703134cc3fc3c21481
Azmetov Danila <ohstapit@gmail.com>
2021-12-17 16:29:13 +0300

    Scripted auto-commit on change (2021-12-17 16:29:13) by gitwatch.sh

--- main/java/ru/bmstu/rapirapr/azmetov/akka/AkkaApp.java
+++ main/java/ru/bmstu/rapirapr/azmetov/akka/AkkaApp.java
@@ -7,6 +7,10 @@ public class AkkaApp {
             System.err.println(USAGE_ERROR_TEXT);
             System.exit(-1);
         }
+        Props props1 = Props.create(MyActor.class);
+        Props props2 = Props.create(ActorWithArgs.class,
+                () -> new ActorWithArgs("arg"));
+        Props props3 = Props.create(ActorWithArgs.class, "arg");
         ActorSystem system = ActorSystem.create("akkaActorSystem");
         ActorRef actor = system.actorOf(
                 Props.create(StoreActor.class)
