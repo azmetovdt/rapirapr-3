846a3bd98e28d811ba0c49c5bd1d1ea8bbd12547
Azmetov Danila <ohstapit@gmail.com>
2021-12-17 16:52:37 +0300

    Scripted auto-commit on change (2021-12-17 16:52:37) by gitwatch.sh

--- src/main/java/bmstu/rapirapr/azmetov/akka/StoreActor.java
+++ src/main/java/bmstu/rapirapr/azmetov/akka/StoreActor.java
@@ -1,5 +1,11 @@
 package bmstu.rapirapr.azmetov.akka;
 
+import akka.actor.AbstractActor;
+
 public class StoreActor extends AbstractActor {
 
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
