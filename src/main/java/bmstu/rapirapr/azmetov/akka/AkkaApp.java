dc9bfc5a41d616298fae382db9e1b2a83612038b
Azmetov Danila <ohstapit@gmail.com>
2021-12-17 17:42:13 +0300

    Scripted auto-commit on change (2021-12-17 17:42:13) by gitwatch.sh

--- src/main/java/bmstu/rapirapr/azmetov/akka/AkkaApp.java
+++ src/main/java/bmstu/rapirapr/azmetov/akka/AkkaApp.java
@@ -4,12 +4,16 @@ import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.server.Route;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
+
+import java.util.concurrent.CompletionStage;
 
 import static akka.http.javadsl.server.Directives.*;
 
@@ -34,8 +38,15 @@ public class AkkaApp {
 
         Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = app.createRoute(actor).flow(system, materializer);
 
-
         Http http = Http.get(system);
+
+        CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", 8080),
+                materializer
+        );
+
+
 
 
     }
