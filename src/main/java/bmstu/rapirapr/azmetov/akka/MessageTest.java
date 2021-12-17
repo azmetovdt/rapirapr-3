384e8657c5d9ee2187cd4a9b004a29f2fbccd4d1
Azmetov Danila <ohstapit@gmail.com>
2021-12-17 19:58:37 +0300

    Scripted auto-commit on change (2021-12-17 19:58:37) by gitwatch.sh

--- src/main/java/bmstu/rapirapr/azmetov/akka/MessageTest.java
+++ src/main/java/bmstu/rapirapr/azmetov/akka/MessageTest.java
@@ -1,43 +1,27 @@
 package bmstu.rapirapr.azmetov.akka;
 
 public class MessageTest {
-    private final String packageId;
-    private final String jsScript;
-    private final String functionName;
+    private final Message message;
     private final Test test;
-
-    public MessageTest(
-            String packageId,
-            String jsScript,
-            String funcName,
-            Test test
-    ) {
-        this.packageId = packageId;
-        this.jsScript = jsScript;
-        this.functionName = funcName;
-        this.test = test;
-    }
 
     public MessageTest(
             Message message,
             Test test
     ) {
-        this.packageId = message.getPackageId();
-        this.jsScript = message.getJsScript();
-        this.functionName = message.getFunctionName();
+        this.message = message;
         this.test = test;
     }
 
     public String getPackageId() {
-        return this.packageId;
+        return this.message.getPackageId();
     }
 
     public String getJsScript() {
-        return this.jsScript;
+        return this.message.getJsScript();
     }
 
     public String getFunctionName() {
-        return this.functionName;
+        return this.message.getFunctionName();
     }
 
     public Test getTest() {
