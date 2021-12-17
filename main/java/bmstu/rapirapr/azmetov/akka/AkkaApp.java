30c4012b691ecd3d70f4fa24b187a3021fc402c1
Azmetov Danila <ohstapit@gmail.com>
2021-12-17 16:46:09 +0300

    Scripted auto-commit on change (2021-12-17 16:46:09) by gitwatch.sh

--- main/java/bmstu/rapirapr/azmetov/akka/AkkaApp.java
+++ main/java/bmstu/rapirapr/azmetov/akka/AkkaApp.java
@@ -2,9 +2,8 @@ package bmstu.rapirapr.azmetov.akka;
 import akka.actor.ActorSystem;
 
 public class AkkaApp {
-
+    public static final String ACTOR_SYSTEM_NAME = "AkkaActorSystem";
     public static void main(String[] args) throws Exception {
-        public static final String ACTOR_SYSTEM_NAME = "AkkaActorSystem";
 //        if (args.length != 3) {
 //            System.err.println(USAGE_ERROR_TEXT);
 //            System.exit(-1);
