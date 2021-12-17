2e1f9dc7b2644dc24a3ebed46db0a2e43a6a26b7
Azmetov Danila <ohstapit@gmail.com>
2021-12-17 18:21:06 +0300

    Scripted auto-commit on change (2021-12-17 18:21:06) by gitwatch.sh

---
+++ src/main/java/bmstu/rapirapr/azmetov/akka/Test.java
@@ -0,0 +1,43 @@
+package bmstu.rapirapr.azmetov.akka;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+public class Test {
+    private final String packageId;
+    private final String jsScript;
+    private final String funcName;
+    private final List<String> tests;
+
+    @JsonCreator
+    public Test(
+            @JsonProperty("packageId") String packageId,
+            @JsonProperty("jsScript") String jsScript,
+            @JsonProperty("functionName") String funcName,
+            @JsonProperty("tests") List<String> tests
+            ) {
+        this.packageId = packageId;
+        this.jsScript = jsScript;
+        this.funcName = funcName;
+        this.tests = tests;
+    }
+
+    public String getPackageId() {
+        return this.packageId;
+    }
+
+    public String getJsScript() {
+        return this.jsScript;
+    }
+
+    public String getFuncName() {
+        return this.funcName;
+    }
+
+    public List<String> getTests() {
+        return this.tests;
+    }
+
+}
